12cfdb7e36c62a88decff03859e950924677a335
Natthasath Saksupanara <natthasath.sak@gmail.com>
2022-12-06 09:37:34 +0700

    Update tag.py

--- app/tag.py
+++ app/tag.py
@@ -8,41 +8,17 @@ def Tags():
                 "url": "https://testdriven.io/blog/fastapi-jwt-auth/",
             },
         },
-        {
-            "name": "EMAIL",
-            "description": "Sending emails and attachments with **Fastapi-mail** for individual && bulk",
-            "externalDocs": {
-                "description": "Items external docs",
-                "url": "https://sabuhish.github.io/fastapi-mail/",
-            },
-        },
     ]
     return tags_metadata
 
 def SubTags():
     tags_metadata = [
-        {
-            "name": "SEC",
-            "description": "Information fund and license with **Open API** for computing && statistic",
+         {
+            "name": "AUTHENTICATION",
+            "description": "Authentication using JSON Web Tokens (JWT) with **PyJWT** for sign, encode, and decode",
             "externalDocs": {
                 "description": "Items external docs",
-                "url": "https://api-portal.sec.or.th/",
-            },
-        },
-        {
-            "name": "OPEND",
-            "description": "Information fund and license with **Open API** for computing && statistic",
-            "externalDocs": {
-                "description": "Items external docs",
-                "url": "https://opend.data.go.th/",
-            },
-        },
-        {
-            "name": "NDID",
-            "description": "Information National Digital ID with **Open API** for computing && statistic",
-            "externalDocs": {
-                "description": "Items external docs",
-                "url": "https://ndidplatform.github.io/",
+                "url": "https://testdriven.io/blog/fastapi-jwt-auth/",
             },
         },
     ]
